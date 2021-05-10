752ed8babed4dfdb0a5afd381e5353c7f969445a
Bruna do Valle Sóre <brunadovallesore@gmail.com>
2021-05-10 12:16:35 -0300

    Criação de métodos nas classes de exception.

--- ProjetoFinalJava/src/Exception/DependenteException.java
+++ ProjetoFinalJava/src/Exception/DependenteException.java
@@ -2,9 +2,36 @@ package Exception;
 
 public class DependenteException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public DependenteException(String message) {
 		super(message);
 		
 	}
+
+	public DependenteException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DependenteException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DependenteException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DependenteException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 }
